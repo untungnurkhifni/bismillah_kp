bb4f2b468d7b0606f070bb34bb798038785a76c9
fukou <faluvtha@gmail.com>
2019-07-15 22:03:07 +0700

    Added a folder named Vue.js with its own files

---
+++ views/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN </span>
+      </v-toolbar-title> 
+      <v-spacer></v-spacer>
+      <router-link to="/">
+      <v-btn flat>
+        <span class="mr-2">Home </span>
+      </v-btn>
+      </router-link>
+
+      <router-link to="/post">
+      <v-btn flat>
+        <span class="mr-2">Post</span>
+      </v-btn>
+      </router-link>
+
+    <router-link to="/user">
+      <v-btn flat>
+        <span class= "mr-2">User</span>
+      </v-btn>
+      </router-link>
+    
+    
+    </v-toolbar>
+
+    <v-content>
+     <router-view/>
+     
+    </v-content>
+  </v-app>
+</template>
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
